405841f405609e8277d6480d61e84040c91dedf1
fatimakofo <fatima26884646@gmail.com>
2023-05-21 15:30:04 +0100

    update

--- user/user.js
+++ user/user.js
@@ -1,10 +1,15 @@
 let sideNav = document.querySelector(".sidenav");
 let close = document.getElementById("close");
 let open = document.getElementById("ham");
+console.log(sideNav, close, open);
 
 console.log(sideNav);
-// console.log("hello");
-open.addEventListener("click", () =>{
-sideNav.classList.toggle('side')
-console.log(sideNav.classList)
-})
+console.log("hello");
+open.addEventListener("click", () => {
+  sideNav.classList.toggle("side");
+  console.log(sideNav.classList);
+});
+
+close.addEventListener("click", () => {
+  sideNav.classList.remove("side");
+});
